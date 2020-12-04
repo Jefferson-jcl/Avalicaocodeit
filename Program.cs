c821367e027f144c99694a9f87b0275f646868c4
Jefferson-jcl <75288049+Jefferson-jcl@users.noreply.github.com>
2020-12-04 09:45:07 -0300

    Commit projeto

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Atividade_Técnica
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+            {
+
+            Grupotecnico g1 = new Grupotecnico("Piloto", "ofc1", "ofc2");
+
+            GrupoCabine g2 = new GrupoCabine("Chefe", "com1", "com2");
+
+            GrupoJudicial g3 = new GrupoJudicial("Policial","Presidiario")
+
+
+            Console.WriteLine(g1.ToString());
+
+            Console.WriteLine(g2.ToString());
+
+            Console.WriteLine(g3.ToString());
+
+        }
+
+
+
+
+
+    }
+}
---
+++ Tripulacao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade_Técnica
+{
+   public abstract class Tripulacao
+    {
+        protected string piloto { get; set; }
+
+        protected string ofc1 { get; set; }
+
+        protected string ofc2 { get; set; }
+
+        protected string chefe { get; set;}
+
+        protected string com1 { get; set; }
+
+        protected string com2 { get; set; }
+
+        protected string policial { get; set; }
+
+        protected string presidiario { get; set; }
+    }
+}
